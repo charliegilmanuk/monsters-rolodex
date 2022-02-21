c408feef406d905455bd5e9a628d458ac47ce9f4
Charlie Gilman <charliegilmanuk@gmail.com>
2022-02-21 18:07:46 +0000

    Fix incorrect attribute

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,6 +2,6 @@ import './search-box.styles.css';
 
 export const SearchBox = ({ placeholder, handleChange }) => {
     return (
-            <input class="search-box" type="search" placeholder={placeholder} onChange={handleChange} />
+            <input className="search-box" type="search" placeholder={placeholder} onChange={handleChange} />
     );
 }
